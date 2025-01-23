be3d9dc2c8602adc06609982972a896ebbcb950c
rishcodelib <ri.sbh23@gmail.com>
2025-01-23 21:33:35 +0530

    upadted  versions

--- resume-genie/src/App.js
+++ resume-genie/src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import './App.css';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Home from './components/Home';
 import ResumeDetails from './components/ResumeDetails';
 
-
 function App() {
   return (
-    <Router>
+    <Router basename="/ResumeGenie-frontend">
       <div className="App">
+        <Helmet>
+          <title>Resume Genie</title>
+        </Helmet>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/resume-details" element={<ResumeDetails />} />
-          
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
